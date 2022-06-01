1ec8413ed6babcbfa09c90661cd3f93307cc4441
edlitvinov <edoss@list.ru>
2022-06-01 22:09:15 +0400

    Filter

--- js/script.js
+++ js/script.js
@@ -30,11 +30,11 @@ console.log(obj2);
 // Maping
 // map, forEach
 
-const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function print(el) {
-    console.log(el);
-}
+// function print(el) {
+//     console.log(el);
+// }
 
 // num.forEach(print);  // вызваная функция будит выполняться для каждого элемента массива;
 /*
@@ -43,9 +43,43 @@ function dubNum(n) {
 }
 */
 
-const res = num.map(n => n * 2);
+// const res = num.map(n => n * 2);
 
+// console.log(num);
+// console.log(res);
+
+// Filter
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const use = [           // коллекция
+{
+    id: 1,
+    name: 'Brad',
+    isMarried: true 
+},
+{
+    id: 2,
+    name: 'Diana',
+    isMarried: false
+},
+{
+    id: 3,
+    name: 'Olivie',
+    isMarried: true
+},
+{
+    id: 4,
+    name: 'Scott',
+    isMarried: false
+}
+];
+
+const marrUse = use.filter(use => use.isMarried);
+console.log(marrUse);
+
+const oddNum = num.filter(n => n % 2 !== 0);
+const evNum = num.filter(n => n % 2 === 0);
+
+console.log(oddNum);
+console.log(evNum);
 console.log(num);
-console.log(res);
-
-
